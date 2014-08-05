69a2539c2b63f209b8a88089e37512d89fbf3d80
Matthew Perry <perrygeo@gmail.com>
2014-08-05 16:31:37 -0700

    increment version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class PyTest(TestCommand):
 
 setup(
     name="pyimpute",
-    version="0.0.2",
+    version="0.0.3",
     author="Matthew Perry",
     author_email="perrygeo@gmail.com",
     description=("Utilities for applying scikit-learn to spatial datasets"),
